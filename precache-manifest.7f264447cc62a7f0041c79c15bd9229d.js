6037af8f2ec72350411319cc42e5dd7e45568b6f
Developer <developer@Dev.local>
2021-11-25 10:41:53 +0800

    Updates

---
+++ precache-manifest.7f264447cc62a7f0041c79c15bd9229d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fced08025f296e48d6d3ace64a46f1df",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "41b6bcc30753ec62f914",
+    "url": "/react-crud/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "d05f27363fdc6504c100",
+    "url": "/react-crud/static/css/main.d40bad5f.chunk.css"
+  },
+  {
+    "revision": "41b6bcc30753ec62f914",
+    "url": "/react-crud/static/js/2.b3aa70ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-crud/static/js/2.b3aa70ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05f27363fdc6504c100",
+    "url": "/react-crud/static/js/main.3d4fffc9.chunk.js"
+  },
+  {
+    "revision": "e0a16fb05aed5a898e3c",
+    "url": "/react-crud/static/js/runtime-main.dfaa1662.js"
+  }
+]);
